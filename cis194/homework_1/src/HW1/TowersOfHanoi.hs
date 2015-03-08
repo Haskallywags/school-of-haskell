4bc8360e34fa2e6600f4fdc6e9c9b8357bd8aaaf
Jon Childress <jon@childr.es>
2015-03-08 06:44:40 -0500

    add Towers of Hanoi specs for homework 1

--- cis194/homework_1/src/HW1/TowersOfHanoi.hs
+++ cis194/homework_1/src/HW1/TowersOfHanoi.hs
@@ -1,6 +1,6 @@
 module HW1.TowersOfHanoi (hanoi) where
 
-type Peg = String
+type Peg = Char
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
---
+++ cis194/homework_1/test/HW1/TowersOfHanoiSpec.hs
@@ -0,0 +1,15 @@
+module TowersOfHanoiSpec (main, spec) where
+
+import HW1.TowersOfHanoi
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec =
+
+  describe "hanoi" $
+
+    it "returns a list of moves required to stack pegs in order" $
+      hanoi 2 'a' 'b' 'c' `shouldBe` [('a','c'), ('a','b'), ('c','b')]
