e184f75f45d9a5f9419a578ee1376a290ea7f5d5
Jon Childress <jon@childr.es>
2015-04-20 13:31:47 -0500

    finished my maybe-sorta-correct version of cis194 hw 4

--- cis194/homework-4/src/HW4/HigherOrder.hs
+++ cis194/homework-4/src/HW4/HigherOrder.hs
@@ -1,9 +1,19 @@
 module HW4.HigherOrder
-( fun1
+( Tree (..)
+, fun1
 , fun2
+, foldTree
+, xor
+, map'
+, myFoldl
+, sieveSundaram
 ) where
 
-import Data.List (unfoldr)
+import Data.List (foldl', unfoldr, filter, notElem)
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+            deriving (Show, Eq)
 
 fun1 :: [Integer] -> Integer
 fun1 = product . map (flip (-) 2) . filter even
@@ -15,4 +25,50 @@ fun2 = sum . unfoldr wat
           | even n    = Just (n, n `div` 2)
           | otherwise = Just (0, 3 * n + 1)
 
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
 
+xor :: [Bool] -> Bool
+xor = foldr (const not) False . filter id
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x ys -> f x : ys) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f = foldr (flip f)
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n =
+    2 : keeps
+  where
+    deletes = map (\(x,y) -> x+y+2*x*y) $ cartProd [1..n] [1..n]
+    keeps = map (\x -> 2*x+1) $ filter (flip notElem deletes) [1..n]
+
+-- private functions
+
+cartProd :: [a] -> [b] -> [(a,b)]
+cartProd xs ys = do
+    x <- xs
+    y <- ys
+    return (x,y)
+
+insert :: a -> Tree a -> Tree a
+insert x Leaf = Node 0 Leaf x Leaf
+insert x (Node _ left x' right)
+    | weight left < weight right =
+      let left' = insert x left
+      in  Node (newHeight left' right) left' x' right
+    | otherwise =
+      let right' = insert x right
+      in  Node (newHeight left right') left x' right'
+
+height :: Tree a -> Integer
+height (Node h _ _ _) = h + 1
+height _              = 0
+
+weight :: Tree a -> Integer
+weight Leaf = 0
+weight (Node _ left _ right) = 1 + weight left + weight right
+
+newHeight :: Tree a -> Tree a-> Integer
+newHeight l r = maximum [height l, height r]
--- cis194/homework-4/test/HW4/HigherOrderSpec.hs
+++ cis194/homework-4/test/HW4/HigherOrderSpec.hs
@@ -1,7 +1,10 @@
 module HW4.HigherOrderSpec (main, spec) where
 
+import Data.List (sort)
+import HW4.HigherOrder (Tree (..))
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Function
 
 import qualified HW4.HigherOrder as HW4
 
@@ -17,6 +20,23 @@ fun2 n
     | even n = n + fun2 (n `div` 2)
     | otherwise = fun2 (3 * n + 1)
 
+treeHeightForList :: [Int] -> Integer
+treeHeightForList l = ceiling . logBase (2 :: Float) . fromIntegral $ length l + 1
+
+heightForTree :: Tree Int -> Integer
+heightForTree (Node h _ _ _) = h + 1
+heightForTree _              = 0
+
+isBalanced :: Tree Int -> Bool
+isBalanced Leaf = True
+isBalanced (Node _ left _ right) =  isBalanced left
+                                 && isBalanced right
+                                 && abs (heightForTree left - heightForTree right) <= 1
+
+treeEles :: Tree Int -> [Int]
+treeEles Leaf = []
+treeEles (Node _ left x right) = treeEles left ++ [x] ++ treeEles right
+
 main :: IO ()
 main = hspec spec
 
@@ -26,9 +46,45 @@ spec = do
   describe "fun1" $
 
     it "has equal results to the predefined fun1" $
-       property $ \ns -> HW4.fun1 ns == fun1 ns
+      property $ \ns -> HW4.fun1 ns == fun1 ns
 
   describe "fun2" $
 
     it "has equal results to the predefined fun2" $
       property $ \(Positive n) -> HW4.fun2 n == fun2 n
+
+  describe "foldTree" $ do
+
+    it "must result in a balanced tree" $
+      property $ \l -> isBalanced (HW4.foldTree l)
+
+    it "contains the height in the root node" $
+      property $ \l -> heightForTree (HW4.foldTree l) == treeHeightForList l
+
+    it "contains the same elements as the list it folds" $
+      property $ \l -> sort l == (sort . treeEles $ HW4.foldTree l)
+
+  describe "xor" $
+
+    it "returns a bool based on the number of \"True\" values" $ do
+      HW4.xor [False, True, False] `shouldBe` True
+      HW4.xor [False, True, False, False, True] `shouldBe` False
+
+  describe "map'" $ do
+    let sameFuncs :: (Fun Int Int, [Int]) -> Bool
+        sameFuncs (f, xs) = HW4.map' (apply f) xs == map (apply f) xs
+
+    it "behaves exactly as \"map\"" $
+      property sameFuncs
+
+  describe "myFoldl" $ do
+    let sameFuncs :: (Int, [Int]) -> Bool
+        sameFuncs (init, xs) = HW4.myFoldl (-) init xs == foldl (-) init xs
+
+    it "behaves exactly as \"foldl\"" $
+      property sameFuncs
+
+  describe "sieveSundaram" $
+
+    it "returns a list of primes up to 2n+1" $
+      HW4.sieveSundaram 3 `shouldBe` [2,3,5,7]
